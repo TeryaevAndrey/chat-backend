df7079087dd75c7557edfc269356cf6f40b17242
Andrey <teryaev.2004@mail.ru>
2023-02-03 00:09:28 +0300

    fix

--- src/routes/dialog.route.ts
+++ src/routes/dialog.route.ts
@@ -7,42 +7,62 @@ const UserSchema = require("../models/User");
 
 const router = Router();
 
-router.post("/new-dialog", checkAuth, async(req: Request, res: Response) => {
+router.post("/new-dialog", checkAuth, async (req: Request, res: Response) => {
   try {
-    const {comradeId, comradeName}: {comradeId: string; comradeName: string} = req.body;
+    const {
+      comradeId,
+      comradeName,
+    }: { comradeId: string; comradeName: string } = req.body;
     const lastMessage: string = req.lastMessage;
-  
-    const candidate = await DialogSchema.findOne({mainUserId: req.userId || comradeId, comradeId: comradeId || req.userId});
 
-    if(candidate) {
-      return res.json({message: "Такой диалог уже существует", dialogId: candidate._id});
+    const candidate = await DialogSchema.findOne({
+      $or: [{ comradeId: req.userId }, { comradeId: comradeId }],
+    });
+
+    console.log(candidate);
+    //{mainUserId: req.userId || comradeId, comradeId: req.userId || comradeId}
+
+    if (candidate) {
+      return res.json({
+        message: "Такой диалог уже существует",
+        dialogId: candidate._id,
+      });
     }
 
-    const user = await UserSchema.findOne({_id: req.userId});
-  
+    const user = await UserSchema.findOne({ _id: req.userId });
+
     const dialog = await new DialogSchema({
       mainUserId: req.userId,
       comradeId,
       mainUserName: user.name,
       comradeName: comradeName,
-      lastMessage
+      lastMessage,
     });
 
     await dialog.save();
 
-    return res.json({message: "Диалог создан успешно!", dialogId: dialog._id});
-  } catch(err) {
-    return res.status(500).json({message: "Ошибка сервера", err: err.message});
+    return res.json({
+      message: "Диалог создан успешно!",
+      dialogId: dialog._id,
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Ошибка сервера", err: err.message });
   }
 });
 
-router.get("/my-dialogs", checkAuth, async(req: Request, res: Response) => {
+router.get("/my-dialogs", checkAuth, async (req: Request, res: Response) => {
   try {
-    const dialogs = await DialogSchema.find({mainUserId: req.userId});
+    const dialogs = await DialogSchema.find({
+      $or: [{ mainUserId: req.userId }, { comradeId: req.userId }],
+    });
 
-    return res.json({dialogs});
-  } catch(err) {
-    return res.status(500).json({message: "Ошибка сервера", err: err.message});
+    return res.json({ dialogs });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Ошибка сервера", err: err.message });
   }
 });
 
