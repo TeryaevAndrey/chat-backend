09a118a1b229b640cb8f2a0175f3a89dbf6b34dc
Andrey <teryaev.2004@mail.ru>
2023-02-05 19:17:40 +0300

    add-get-messages-route-and-fix

--- src/routes/message.route.ts
+++ src/routes/message.route.ts
@@ -4,23 +4,26 @@ const Router = require("express");
 const expressWs = require("express-ws");
 const MessageSchema = require("../models/Message");
 import {expressWss} from "../index";
+const checkAuth = require("../utils/checkAuth");
 
 const router = Router();
 expressWs(router);
 
 router
   .ws("/new-message/:id", (ws, req: Request) => {
+    const id = req.params.id;
+
     ws.on("connection", () => {
       console.log("Подключено");
     })
 
     ws.on("message", (msg) => {
-      expressWss.getWss("/new-message").clients.forEach(client => {
+      expressWss.getWss(`/new-message/${id}`).clients.forEach(client => {
           client.send(msg);
       });
     });
   })
-  .post("/new-message", async (req: Request, res: Response) => {
+  .post("/new-message",checkAuth, async (req: Request, res: Response) => {
     try {
       const { messageText, dialog }: {
         messageText: string;
@@ -38,7 +41,20 @@ router
 
       return res.json({ message: "Сообщение отправлено" });
     } catch (err) {
+      console.log(err.message);
       return res.status(500).json({ message: "Ошибка сервера" });
+    }
+  });
+
+  router.get("/get-messages/:id", async(req: Request, res: Response) => {
+    try {
+      const dialogId = req.params.id;
+
+      const messages = await MessageSchema.find({dialog: dialogId});
+
+      return res.json({message: "Сообщения, которые мы нашли", messages});
+    } catch(err) {
+      return res.status(500).json({message: "Ошибка сервера", err: err.message});
     }
   });
 
