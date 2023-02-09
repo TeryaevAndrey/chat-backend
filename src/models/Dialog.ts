d3402124b8820e375b91b6c4b3f5aa8272918a91
Andrey <teryaev.2004@mail.ru>
2023-02-09 14:09:59 +0300

    prettier-and-add-password-for-models-user

--- src/models/Dialog.ts
+++ src/models/Dialog.ts
@@ -10,11 +10,11 @@ interface IDialogSchema {
 
 const DialogSchema = new Schema<IDialogSchema>(
   {
-    creator: {type: String, required: true},
-    fellow: {type: String, required: true},
-    creatorName: {type: String, required: true},
-    fellowName: {type: String, required: true},
-    lastMessage: {type: String}
+    creator: { type: String, required: true },
+    fellow: { type: String, required: true },
+    creatorName: { type: String, required: true },
+    fellowName: { type: String, required: true },
+    lastMessage: { type: String },
   },
   { timestamps: true }
 );
--- src/models/User.ts
+++ src/models/User.ts
@@ -3,16 +3,18 @@ import { model, Schema } from "mongoose";
 interface IUserSchema {
   avatar?: string;
   userName: string;
+  password: string;
   isOnline: boolean;
   wasOnline?: string;
 }
 
 const UserSchema = new Schema<IUserSchema>(
   {
-    avatar: {type: String}, 
-    userName: {type: String, required: true}, 
-    isOnline: {type: Boolean, required: true}, 
-    wasOnline: {type: String}
+    avatar: { type: String },
+    userName: { type: String, required: true },
+    password: {type: String, required: true},
+    isOnline: { type: Boolean, required: true },
+    wasOnline: { type: String },
   },
   { timestamps: true }
 );
