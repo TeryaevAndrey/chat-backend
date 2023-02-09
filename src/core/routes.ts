2b14825f01639ba734b0525c7693d06611cf9cf7
Andrey <teryaev.2004@mail.ru>
2023-02-09 23:33:53 +0300

    rework-messages-controller

--- src/core/routes.ts
+++ src/core/routes.ts
@@ -1,21 +1,23 @@
 import { Express } from "express";
 import AuthCtrl from "../controllers/AuthController";
 import UsersCtrl from "../controllers/UsersController";
-import DialogCtrl from "../controllers/dialogController";
+import DialogsCtrl from "../controllers/DialogsController";
+import MessagesCtrl from "../controllers/MessagesController";
 
 const createRoutes = (app: Express) => {
   const AuthController = new AuthCtrl();
   const UsersController = new UsersCtrl();
-  const DialogController = new DialogCtrl();
+  const DialogsController = new DialogsCtrl();
+  const MessagesController = new MessagesCtrl();
 
   app.post("/api/auth/reg", AuthController.reg);
   app.post("/api/auth/entrance", AuthController.entrance);
   app.post("/api/users/users-search", UsersController.usersSearch);
   app.get("/api/users/get-all-users", UsersController.getAllUsers);
-  app.post("/api/dialogs/new-dialog", DialogController.newDialog);
-  app.get("/api/dialogs/get-my-dialogs", DialogController.getMyDialogs);
-  // app.post("/api/message/new-message", messageController().newMessage);
-  // app.get("/api/message/get-messages/:id", messageController().getMessages);
+  app.post("/api/dialogs/new-dialog", DialogsController.newDialog);
+  app.get("/api/dialogs/get-my-dialogs", DialogsController.getMyDialogs);
+  app.post("/api/messages/new-message", MessagesController.newMessage,); 
+  app.get("/api/messages/get-messages/:dialogId", MessagesController.getMessages);
 };
 
 export default createRoutes;
